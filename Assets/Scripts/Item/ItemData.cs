17809954aaee0c04a858c0f5a23df477cb3c9787
park8306 <kosaf0804@gmail.com>
2025-03-07 09:56:09 +0900

    [ feat ] 아이템 데이터 생성

---
+++ Assets/Scripts/Item/ItemData.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum ItemType
+{
+    Heal,
+    Jump,
+    Speed
+}
+
+
+[CreateAssetMenu(menuName = "New Item Data")]
+public class ItemData : ScriptableObject
+{
+    public string itemName;     // 아이템 이름
+    public string description;  // 아이템 설명
+    public ItemType type;   // 아이템 타입
+    public float value;     // 타입마다 다르게 적용될 값
+    public float duration;  // 지속 시간
+}
