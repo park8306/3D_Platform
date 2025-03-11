b3ff64ab44ef0e5a9da8254f6283cdbdb0edec5d
park8306 <kosaf0804@gmail.com>
2025-03-11 15:22:59 +0900

    [ chore ] 빌드 안되는 요소 제거

--- Assets/Scripts/UI/DisplayInfoUI.cs
+++ Assets/Scripts/UI/DisplayInfoUI.cs
@@ -1,8 +1,4 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using TMPro;
-using UnityEditor.Rendering.LookDev;
 using UnityEngine;
 
 public class DisplayInfoUI : MonoBehaviour
